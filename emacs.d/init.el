5c578b0df70423be9be3677f466fd410cdd63360
Dario Bertini <berdario@gmail.com>
2012-12-09 08:03:01 +0100

    Added emacs fsharp-mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -121,3 +121,10 @@
 ;;   (interactive)
 ;;   (increase-left-margin START END)
 ;;   (indent-region-copy START END COLUMN))
+
+(setq load-path (cons "~/.emacs.d/packages/fsharp" load-path))
+(setq auto-mode-alist (cons '("\\.fs[iylx]?$" . fsharp-mode) auto-mode-alist))
+(autoload 'fsharp-mode "fsharp" "Major mode for editing F# code." t)
+(autoload 'run-fsharp "inf-fsharp" "Run an inferior F# process." t)
+(setq inferior-fsharp-program "fsharpi")
+(setq fsharp-compiler "fsharpc")
