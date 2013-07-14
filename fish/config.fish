6a8e7b820ab1a4db95163f7a8be1c626b9930f04
Dario Bertini <berdario@gmail.com>
2013-07-14 14:58:44 +0200

    Installed OPAM and Ocaml4

--- fish/config.fish
+++ fish/config.fish
@@ -19,7 +19,12 @@ set -x CLOJURESCRIPT_HOME "$HOME/Projects/clojurescript"
 
 which java > /dev/null ; and set -x JAVA_HOME (readlink -f (echo (dirname (readlink -f (which java)))"/../../."))
 
-set -l additional_paths ~/Applications/bin ~/.rbenv/shims ~/.cabal/bin ~/Applications/depot_tools
+set OCAMLVERSION 4.00.1
+set -x CAML_LD_LIBRARY_PATH ~/.opam/$OCAMLVERSION/lib/stublibs /usr/lib/ocaml/stublibs
+set -x OCAML_TOPLEVEL_PATH ~/.opam/$OCAMLVERSION/lib/toplevel
+set -x MANPATH ~/.opam/$OCAMLVERSION/man:$MANPATH
+
+set -l additional_paths ~/.opam/$OCAMLVERSION/bin ~/Applications/bin ~/.rbenv/shims ~/.cabal/bin ~/Applications/depot_tools
 mkdir -p $additional_paths
 set -x PATH $additional_paths $PATH
 
