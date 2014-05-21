e1dffe80e235717bacf6ecc2a27f93c85125f613
Dario Bertini <berdario@gmail.com>
2014-05-21 19:01:48 +0200

    Add the refresh fn to new repls

--- lein-profile.clj
+++ lein-profile.clj
@@ -6,9 +6,11 @@
             [lein-marginalia "0.7.1"]]
   :dependencies [[ritz/ritz-nrepl-middleware "0.7.0"]
                  [ritz/ritz-debugger "0.7.0"]
-                 [ritz/ritz-repl-utils "0.7.0"]]
+                 [ritz/ritz-repl-utils "0.7.0"]
+                 [org.clojure/tools.namespace "0.2.4"]]
   :repl-options {:nrepl-middleware
                  [ritz.nrepl.middleware.javadoc/wrap-javadoc
-                  ritz.nrepl.middleware.simple-complete/wrap-simple-complete]}}
+                  ritz.nrepl.middleware.simple-complete/wrap-simple-complete]
+                 :init (require '[clojure.tools.namespace.repl :refer [refresh]])}}
  :hooks
  [ritz.add-sources]}
