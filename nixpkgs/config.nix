b86590e6f7f596b7c7bf9d1d9271a025b5573559
Dario Bertini <dario.bertini@nccgroup.com>
2015-04-17 12:43:22 +0100

    Setup, nix: Add fsharp, and handle its conflict with scala due to fsc

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -6,5 +6,10 @@
         sha256 = "7c6a2a4f71e8ab09804e6b4fb3aff998c5583108ac42c0e2967eee8e1dbc7406";
       };
     });
+    # scala = pkgs.stdenv.lib.overrideDerivation pkgs.scala (oldAttrs: {
+    #   meta.priority = "10";
+    # });
+    # TODO: override scala to lower priority and simplify the setup/packages.nix
+    # https://github.com/NixOS/nixpkgs/issues/7425
   };
 }
