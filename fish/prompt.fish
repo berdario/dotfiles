3a3969042736b088b802eac3ad111df6a612de3d
Dario Bertini <berdario@gmail.com>
2015-11-22 18:41:09 +0000

    fish: fish2.2 doesn't support anymore "whatever bold", colors... switch to "--bold whatever"

--- fish/prompt.fish
+++ fish/prompt.fish
@@ -2,7 +2,7 @@ set -g __fish_git_prompt_showdirtystate 1
 set -g __fish_git_prompt_showuntrackedfiles 1
 set -g __fish_git_prompt_showstashstate 1
 
-set -g __fish_git_prompt_color red bold
+set -g __fish_git_prompt_color --bold red
 set -g __fish_git_prompt_char_stateseparator ""
 
 set -g __fish_git_prompt_showupstream informative
